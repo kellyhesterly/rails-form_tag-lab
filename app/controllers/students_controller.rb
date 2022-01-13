c8fb567f53ae41347fc89df9e1b7555f55db1eb3
github username <kelly.hesterly@gmail.com>
2022-01-12 20:24:51 -0700

    commit tree f9efc914ecd16b6f992f5bf32b1ccff17e2a7bfd

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,11 +9,13 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to students_path
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
